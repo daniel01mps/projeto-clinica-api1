d4180b0d4c40152f43c9def40d50c4b0475ab339
unknown <carlospires0794@digitalcollege.internal>
2025-04-30 11:54:45 -0300

    Updade

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,8 +1,21 @@
-const { PrismaClient } = require("../generated/prisma");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
 
 async function criarUsuario(dados) {
     try {
+        let senhaCriptografada = await bcrypt.hash(dados.usuario_senha, 10);
+        dados = { ...dados, usuario_senha: senhaCriptografada };
+        const checarEmail = await prisma.usuario.count({
+            where: {
+                usuario_email: dados.usuario_email
+            }
+        })
+
+        if (checarEmail > 0) {
+            throw new Error("Este email já está cadastrado")
+        }
+
         const request = await prisma.usuario.create({
             data: dados
         })
@@ -24,6 +37,38 @@ async function criarUsuario(dados) {
     }
 }
 
+
+async function login(dados) {
+    try {
+        const usuario = await prisma.usuario.findFirst({
+            where: {
+                usuario_email: dados.usuario_email
+            }
+        })
+        const logado = await bcrypt.compare(dados.usuario_senha, usuario.usuario_senha);
+
+        if (logado) {
+            return {
+                type: "sucess",
+                message: "Usuario Logado!"
+            }
+        } else {
+            return {
+                type: "Warning",
+                message: "Email ou senha incorretos"
+            }
+        }
+
+    } catch (error) {
+        return {
+            type: "error",
+            message: error.message
+        }
+    }
+}
+
+
 module.exports = {
-    criarUsuario
+    criarUsuario,
+    login
 }
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,9 +1,13 @@
-const { criarUsuario } = require('../controllers/usuarioController');
+const { criarUsuario, login } = require('../controllers/usuarioController');
 
 const router = require('express').Router();
 
 router.post("/", async (req, res) => {
     res.send(await criarUsuario(req.body));
+})
+
+router.post("/login", async (req, res) => {
+    res.send(await login(req.body));
 })
 
 module.exports = router;
